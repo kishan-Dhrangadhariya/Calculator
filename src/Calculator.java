9cca01e0dd064c844e8971e416a008551f031890
Kishan Dhrangadhariya <dhrangadhariyak@gmail.com>
2020-06-03 20:23:06 +0530

    update-4

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,6 +5,26 @@ public class Calculator {
 		if(numbers.length() == 0) {
 			return 0;
 		}
+		else if(numbers.charAt(0) == '/' && numbers.charAt(1) == '/') {
+			int in = numbers.indexOf("\n");
+			String del = numbers.substring(2, 3);
+			
+			numbers = numbers.substring(in+1, numbers.length());
+			String[] split = numbers.split(del);
+			int n = split.length;
+			int sum=0, num;
+			
+			for(int i=0;i<n;i++) {
+				try {
+					num = Integer.parseInt(split[i]);
+					sum += num;
+				} catch (Exception e) {
+					System.out.println("Invalid String");
+					return -1;
+				}
+			}
+			return sum;
+		}
 		else if(numbers.contains(",")) {
 			String del = "[,\n]";
 			String[] split = numbers.split(del);
--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -3,13 +3,16 @@ import static org.junit.Assert.*;
 public class CalculatorTest {
 	@Test
 	 public void testAdd() {
-		int num1 =-1, num2 = -1, num3 = -1, num4 = -1, num5 = -1;
+		int num1 =-1, num2 = -1, num3 = -1, num4 = -1, num5 = -1, num6 = -1, num7 = -1, num8 = -1;
 		try {
 			num1 = Calculator.Add("1,3");
 			num2 = Calculator.Add("");
 			num3 = Calculator.Add("1");
 			num4 = Calculator.Add("1,2,4,5");
 			num5 = Calculator.Add("1,2,4\n5");
+			num6 = Calculator.Add("//:\n1:2:4:5");
+			num7 = Calculator.Add("1,2,4\n5");
+			num8 = Calculator.Add("1,2,4\n5");
 		} catch (Exception e) {
 			((Throwable) e).printStackTrace();
 		}
@@ -18,5 +21,8 @@ public class CalculatorTest {
 		assertEquals(num3, 1);
 		assertEquals(num4, 12);
 		assertEquals(num5, 12);
+		assertEquals(num6, 12);
+		assertEquals(num7, 12);
+		assertEquals(num8, 12);
 	 }
 }
